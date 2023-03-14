0d01e276c2f8f2f77f85dcf0c9c267f030e1b366
jsiggia-38 <jsiggia38@gmail.com>
2023-03-14 02:07:12 -0400

    updates
    updates

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,75 @@ import './App.css';
 const App = () => {
 
   const [index, setIndex] = useState(0);
+  const [input, setInput] = useState("");
+  const [trueAnswer, setTrueAnswer] = useState(FLASHCARDS[0].answer);
+  const [correct_input, setCorrectInput] = useState("");
 
-  const updateIndex = () => setIndex(Math.floor(Math.random() * FLASHCARDS.length));
+  const getNextCard = () => {
+    
+    if (index == (FLASHCARDS.length - 1)) {
+      setIndex(FLASHCARDS.length - 1);
+    }
+
+    else {
+
+      setIndex(index + 1);
+      setTrueAnswer(FLASHCARDS[index + 1].answer);
+      setInput("");
+      setCorrectInput("");
+   
+
+    }
+    
+    
+   
+    
+    
+  };
+
+
+  const getLastCard = () => {
+
+    if (index == 0) {
+      setIndex(0);
+    }
+
+    else {
+      setIndex(index - 1);
+      setTrueAnswer(FLASHCARDS[index - 1].answer);
+      setInput("Type Answer Here...");
+      setCorrectInput("");
+
+    }
+  }
+
+
+  const onCheck = () => {
+
+    if (input == "" || input == " ") {
+      alert("You forgot to type an answer!");
+    }
+
+    else {
+      if (input == trueAnswer) {
+        setCorrectInput("Correct");
+      }
+
+      else {
+        setCorrectInput("Incorrect");
+      }
+    }
+
+  }
+
+  const handleChange = (event) => {
+    const newInput = event.target.value;
+    setInput(newInput);
+
+
+    
+    
+}
 
   return (
 
@@ -22,7 +89,24 @@ const App = () => {
 
       <QuestionCard index = {index} />
 
-      <p className = "next-button-box"><button className = "next-button" onClick = {updateIndex}><img className = "next-button-picture" src = "../images/RightArrow.png"/></button></p>
+     
+
+      <div className = "answer-container">
+
+        <input type = "text" className = "textbox" onChange = {handleChange} id = {correct_input} placeholder = "Type Answer Here..."/>
+
+        <button type = "submit" className = "check-button" onClick = {onCheck}>Check Answer</button>
+        
+      </div>
+
+      <div className = "nav-buttons">
+
+
+        <button className = "prev-button" onClick = {getLastCard}><img className = "prev-button-picture" src = "../images/LeftArrow.png" /></button>
+
+        <button className = "next-button" onClick = {getNextCard}><img className = "next-button-picture" src = "../images/RightArrow.png"/></button>
+
+      </div>
 
 
       
